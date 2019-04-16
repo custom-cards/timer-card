c55c5c07c26a121da223b1c49abef1b16350f0ad
Ian Richardson <iantrich@gmail.com>
2019-04-15 20:35:32 -0500

    Basic Implementation

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-timer-card
+# ⏰ Lovelace Timer Card
+
+[![GitHub Release][releases-shield]][releases]
+[![GitHub Activity][commits-shield]][commits]
+[![custom_updater][customupdaterbadge]][customupdater]
+[![License][license-shield]](LICENSE.md)
+
+![Project Maintenance][maintenance-shield]
+[![BuyMeCoffee][buymecoffeebadge]][buymecoffee]
+
+[![Discord][discord-shield]][discord]
+[![Community Forum][forum-shield]][forum]
+
+This element is for [Lovelace](https://www.home-assistant.io/lovelace) on [Home Assistant](https://www.home-assistant.io/) that that shows a timer and its controls.
+
+<!-- ![example](example.gif) -->
+
+## Options
+
+| Name | Type | Requirement | Description | Default
+| ---- | ---- | ------- | ----------- | -------
+| type | string | **Required** | `custom:timer-card` | `none`
+| entity | string | **Required** | Timer entity id | `none`
+| name | string | **Optional** | Tooltip for main menu | `none`
+
+## Installation
+
+### Step 1
+
+Save [timer-card](https://github.com/custom-cards/timer-card/raw/master/dist/timer-card.js) to `<config directory>/www/timer-card.js` on your Home Assistant instanse.
+
+**Example:**
+
+```bash
+wget https://raw.githubusercontent.com/custom-cards/timer-card/master/dist/timer-card.js
+mv timer-card.js /config/www/
+```
+
+### Step 2
+
+Link `timer-card` inside your `ui-lovelace.yaml` or Raw Editor in the UI Editor
+
+```yaml
+resources:
+  - url: /local/timer-card.js
+    type: module
+```
+
+### Step 3
+
+Add a custom element in your `ui-lovelace.yaml` or in the UI Editor as a Manual Card
+
+```yaml
+type: 'custom:timer-card'
+icon: 'timer.laundry'
+name: 'Laundry'
+```
+
+[Troubleshooting](https://github.com/thomasloven/hass-config/wiki/Lovelace-Plugins)
+
+Inspiration taken from [Creative Punch](https://codepen.io/CreativePunch/pen/lAHiu)
+
+[buymecoffee]: https://www.buymeacoffee.com/iantrich
+[buymecoffeebadge]: https://img.shields.io/badge/buy%20me%20a%20coffee-donate-blue.svg?style=for-the-badge
+[commits-shield]: https://img.shields.io/github/commit-activity/y/custom-cards/timer-card.svg?style=for-the-badge
+[commits]: https://github.com/custom-cards/timer-card/commits/master
+[customupdater]: https://github.com/custom-components/custom_updater
+[customupdaterbadge]: https://img.shields.io/badge/custom__updater-true-success.svg?style=for-the-badge
+[discord]: https://discord.gg/Qa5fW2R
+[discord-shield]: https://img.shields.io/discord/330944238910963714.svg?style=for-the-badge
+[forum-shield]: https://img.shields.io/badge/community-forum-brightgreen.svg?style=for-the-badge
+[forum]: https://community.home-assistant.io
+[license-shield]: https://img.shields.io/github/license/custom-cards/timer-card.svg?style=for-the-badge
+[maintenance-shield]: https://img.shields.io/badge/maintainer-Ian%20Richardson%20%40iantrich-blue.svg?style=for-the-badge
+[releases-shield]: https://img.shields.io/github/release/custom-cards/timer-card.svg?style=for-the-badge
+[releases]: https://github.com/custom-cards/timer-card/releases
---
+++ src/timer-card.ts
@@ -0,0 +1,164 @@
+import {
+  LitElement,
+  html,
+  customElement,
+  property,
+  TemplateResult,
+  css,
+  CSSResult,
+  PropertyValues
+} from "lit-element";
+
+import { TimerCardConfig, HomeAssistant } from "./types";
+import { HassEntity } from "home-assistant-js-websocket";
+import secondsToDuration from "./seconds_to_duration";
+import timerTimeRemaining from "./timer_time_remaining";
+import { hasConfigOrEntityChanged } from "./has-changed";
+import durationToSeconds from "./duration_to_seconds";
+import { fireEvent } from "./fire_event";
+
+@customElement("timer-card")
+class RadialMenu extends LitElement {
+  @property() public hass?: HomeAssistant;
+
+  @property() private _config?: TimerCardConfig;
+
+  @property() private _timeRemaining?: number;
+
+  private _interval?: number;
+
+  public setConfig(config: TimerCardConfig): void {
+    if (!config || !config.entity) {
+      throw new Error("Invalid configuration");
+    }
+
+    this._config = config;
+  }
+
+  public disconnectedCallback(): void {
+    super.disconnectedCallback();
+    this._clearInterval();
+  }
+
+  public getCardSize(): number {
+    return 1;
+  }
+
+  protected render(): TemplateResult | void {
+    if (!this._config || !this.hass) {
+      return html``;
+    }
+
+    const stateObj = this.hass.states[this._config.entity];
+
+    if (!stateObj) {
+      return html`
+        <hui-warning
+          >${this.hass.localize(
+            "ui.panel.lovelace.warning.entity_not_found",
+            "entity",
+            this._config.entity
+          )}</hui-warning
+        >
+      `;
+    }
+
+    return html`
+      <ha-card .header="${this._config.name}">
+        <div @click="${this._moreInfo}">${this._computeDisplay(stateObj)}</div>
+        <paper-icon-button
+          .action="${stateObj.state === "active" ? "pause" : "start"}"
+          @click="${this._handleClick}"
+          .icon="${stateObj.state === "active" ? "mdi:pause" : "mdi:play"}"
+          .title="${stateObj.state === "active" ? "Pause" : "Start"}"
+        ></paper-icon-button>
+        <paper-icon-button
+          .action="${"cancel"}"
+          @click="${this._handleClick}"
+          icon="mdi:timer-off"
+          title="Cancel"
+        ></paper-icon-button>
+      </ha-card>
+    `;
+  }
+
+  protected shouldUpdate(changedProps: PropertyValues): boolean {
+    if (changedProps.has("_timeRemaining")) {
+      return true;
+    }
+
+    return hasConfigOrEntityChanged(this, changedProps);
+  }
+
+  protected updated(changedProps: PropertyValues) {
+    super.updated(changedProps);
+
+    if (changedProps.has("hass")) {
+      const stateObj = this.hass!.states[this._config!.entity];
+      const oldHass = changedProps.get("hass") as this["hass"];
+      const oldStateObj = oldHass
+        ? oldHass.states[this._config!.entity]
+        : undefined;
+
+      if (oldStateObj !== stateObj) {
+        this._startInterval(stateObj);
+      } else if (!stateObj) {
+        this._clearInterval();
+      }
+    }
+  }
+
+  private _clearInterval(): void {
+    if (this._interval) {
+      window.clearInterval(this._interval);
+      this._interval = undefined;
+    }
+  }
+
+  private _startInterval(stateObj: HassEntity): void {
+    this._clearInterval();
+    this._calculateRemaining(stateObj);
+
+    if (stateObj.state === "active") {
+      this._interval = window.setInterval(
+        () => this._calculateRemaining(stateObj),
+        1000
+      );
+    }
+  }
+
+  private _calculateRemaining(stateObj: HassEntity): void {
+    this._timeRemaining = timerTimeRemaining(stateObj);
+  }
+
+  private _computeDisplay(stateObj: HassEntity): string | null {
+    if (!stateObj) {
+      return null;
+    }
+
+    return secondsToDuration(
+      this._timeRemaining || durationToSeconds(stateObj.attributes["duration"])
+    );
+  }
+
+  private _moreInfo(): void {
+    fireEvent(this, "hass-more-info", {
+      entityId: this._config!.entity
+    });
+  }
+
+  private _handleClick(ev) {
+    this.hass!.callService("timer", ev.currentTarget.action, {
+      entity_id: this._config!.entity
+    });
+  }
+
+  static get styles(): CSSResult {
+    return css`
+      div {
+        padding-left: 16px;
+        font-size: 24px;
+      }
+    `;
+  }
+}
